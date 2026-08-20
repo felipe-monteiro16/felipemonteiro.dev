a96ee68772a34321ee5c6779990416336254572b
Felipe Monteiro <Felipe Monteiro>
2026-08-20 20:51:23 -0300

    feat: atividade 03 da disciplina de programação ii

---
+++ unimontes/programacao-ii/atividade-03/script.js
@@ -0,0 +1,20 @@
+EMAIL = "admin@email.com"
+PASSWORD = "admin"
+
+function postLogin(){
+    event.preventDefault();
+    email = document.getElementById("email").value
+    password = document.getElementById("password").value
+
+    if (email == EMAIL && password == PASSWORD) {
+        document.getElementById("output_div").className = "success-message"
+        document.getElementById("output_div").innerText = "Login realizado com sucesso!"
+
+    } else {
+        document.getElementById("output_div").className = "error-message"
+        document.getElementById("output_div").innerText = "Credenciais Invalidas."
+
+    }
+
+
+}
